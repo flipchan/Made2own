4c7143a13178db08bf5c2a4af2bc5bf5b50d5585
root <root@fedora-23-dvm>
2017-03-20 20:09:18 +0000

    rick roll

---
+++ ATtiny_85/rickroll.ino
@@ -0,0 +1,13 @@
+#include <DigiKeyboard.h>
+
+//rickrolls the user every15 seconds
+
+void setup(){
+
+}
+
+void loop(){
+  DigiKeyboard.println("cmd.exe /C start https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+  delay(1500)
+}
+//cmd.exe /C start
